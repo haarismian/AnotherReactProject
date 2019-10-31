ad07d8768959f518919f8ef4d34135a0d3ae4703
Haaris Mian <mail@haarismian.com>
2019-10-30 23:16:31 -0400

    small comment change

--- ReactCrudDemo/Controllers/EmployeeController.cs
+++ ReactCrudDemo/Controllers/EmployeeController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ReactCrudDemo.Models;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ReactCrudDemo.Controllers
 {
     public class EmployeeController : Controller
